1b3bd0919bddfd0a61c138ded1318320663410ae
fjohnson1991 <felicity.johnson.mail@gmail.com>
2016-10-05 16:38:59 -0400

    commit

--- HellaCells/ViewController.swift
+++ HellaCells/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
 
+    var numArray = [Int]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        numbersArray()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "basicCell", for: indexPath)
+        cell.textLabel?.text = String(numArray[indexPath.row])
+        return cell
+    }
+    
+    func numbersArray() {
+        for i in 0...100 {
+            numArray.append(i)
+        }
+    }
 
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "hellaDetail" {
+            if let dest = segue.destination as? HellaDetailViewController {
+                if let indexPath = tableView.indexPathForSelectedRow {
+                    dest.numLabel = String(numArray[indexPath.row])
+                }
+            }
+            
+        }
+    }
+    
 }
-
